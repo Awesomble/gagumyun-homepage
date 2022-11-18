518731ae7daeb4947eda8d8191f4928697f2a13b
aosdinkim <master@awesomble.com>
2022-11-18 15:20:48 +0900

    tst

---
+++ app/ssr/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+export default function Page() {
+    return (
+        <>
+            <h1>Server-side rendered page</h1>
+            <p>
+                This page is server-side rendered. It was rendered on.
+            </p>
+            <p>
+                <a href="/Users/aosdinkim/Documents/project/gagumyun-homepage/pages">View a static page.</a>
+            </p>
+        </>
+    );
+}
