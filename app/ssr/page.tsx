86e9a00ae20364813b4ea1c929bf10fb876e1992
aosdinkim <master@awesomble.com>
2022-11-18 15:28:36 +0900

    tst

--- app/ssr/page.tsx
+++ app/ssr/page.tsx
@@ -1,11 +1,23 @@
 'use client'
+function getServerSideProps() {
+    const renderDate = Date.now();
+    const formattedDate = new Intl.DateTimeFormat("en-US", {
+        dateStyle: "long",
+        timeStyle: "long",
+    }).format(renderDate);
+    console.log(
+        `SSR ran on ${formattedDate}. This will be logged in CloudWatch.`
+    );
+    return { props: { formattedDate } };
+}
 
 export default function Page() {
+    const formattedDate = getServerSideProps().props.formattedDate
     return (
         <>
             <h1>Server-side rendered page</h1>
             <p>
-                This page is server-side rendered. It was rendered on.
+                This page is server-side rendered. It was rendered on {formattedDate}.
             </p>
             <p>
                 <a href="/Users/aosdinkim/Documents/project/gagumyun-homepage/pages">View a static page.</a>
@@ -13,3 +25,4 @@ export default function Page() {
         </>
     );
 }
+
